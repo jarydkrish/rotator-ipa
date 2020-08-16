fbe6d71229989b618bd49637345520bd080d8076
Jaryd Krishnan <jaryd@harvestprofit.com>
2020-08-15 20:41:01 -0500

    Add everything for pretending to be a real brewing company

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module RotatorIpa
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators.assets = false
+    config.generators.helper = false
   end
 end
