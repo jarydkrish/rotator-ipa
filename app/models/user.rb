ff53acc2cb7c16d7f1e78145fbba4c33a87359d1
Jaryd Krishnan <jaryd@harvestprofit.com>
2020-08-30 17:32:42 -0500

    Ensure we do not use the Cloudflare IP address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_many :posts
   # Include default devise modules. Others available are:
@@ -7,4 +9,10 @@ class User < ApplicationRecord
          :lockable, :timeoutable, :trackable
 
   validates :name, presence: true
+
+  protected
+
+  def extract_ip_from(request)
+    request.headers['X-Forwarded-For'] || request.remote_ip
+  end
 end
