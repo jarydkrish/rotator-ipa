c4dafe6837de4f61ebf658384e1219055d911dc3
Jaryd Krishnan <jarydkrishnan@gmail.com>
2020-12-06 19:09:57 -0600

    Add code coverage

--- Gemfile
+++ Gemfile
@@ -47,5 +47,6 @@ end
 group :test do
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
+  gem 'simplecov', require: false
   gem 'webdrivers'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_group 'Dashboards', 'app/dashboards'
+  add_group 'Fields', 'app/fields'
+  add_group 'GraphQL', 'app/graphql'
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
@@ -7,7 +14,7 @@ require 'rails/test_help'
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    parallelize(workers: 1)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
