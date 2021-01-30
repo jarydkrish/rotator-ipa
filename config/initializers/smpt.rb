cc61eceb9313d10dbee372afdc41357935c63271
Jaryd Krishnan <jaryd@hey.com>
2021-01-30 17:18:02 -0600

    Ensure we set SMTP settings correctly

--- config/initializers/smpt.rb
+++ config/initializers/smpt.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: 587,
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
-  authentication: :login,
-  enable_starttls_auto: true
-}
+ActiveSupport.on_load(:action_mailer) do
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    authentication: :login,
+    enable_starttls_auto: true
+  }
+end
