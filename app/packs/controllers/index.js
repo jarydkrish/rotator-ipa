b112071ae21ccbf434328c958ebe5c95f776f2aa
Jaryd Krishnan <jaryd@hey.com>
2021-02-13 19:58:16 -0600

    Fix up mobile navigation

--- app/packs/controllers/index.js
+++ app/packs/controllers/index.js
@@ -3,8 +3,11 @@
 
 import { Application } from 'stimulus';
 import { definitionsFromContext } from 'stimulus/webpack-helpers';
+import { Dropdown } from 'tailwindcss-stimulus-components';
+
 
 const application = Application.start();
 
 const context = require.context('.', true, /_controller\.js$/);
 application.load(definitionsFromContext(context));
+application.register('dropdown', Dropdown);
