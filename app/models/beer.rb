76b33b851de6d8a08b8136e05badf719611fc333
Jaryd Krishnan <jaryd@hey.com>
2021-03-27 17:15:56 -0500

    Remove unused methods

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -10,26 +10,6 @@ class Beer < ApplicationRecord
   has_many :beer_hourly_data_points, dependent: :destroy
   has_rich_text :recipe
 
-  def hourly_chart_data
-    beer_hourly_data_points.map do |data_point|
-      {
-        time: data_point.created_at,
-        temperature: data_point.temperature,
-        specific_gravity: data_point.specific_gravity
-      }
-    end
-  end
-
-  def hourly_chart_data_for_carboy(carboy)
-    beer_hourly_data_points.where(carboy: carboy).map do |data_point|
-      {
-        time: data_point.created_at,
-        temperature: data_point.temperature,
-        specific_gravity: data_point.specific_gravity
-      }
-    end
-  end
-
   def most_recent_daily_data_point
     @most_recent_daily_data_point ||= beer_daily_data_points.last
   end
